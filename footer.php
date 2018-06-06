37d2499cf052fd1ad661ba637743dedc9f054e66
pfadarraweb@gmail.com <pfadarraweb@gmail.com>
2018-06-06 16:00:46 +0200

    design/footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
    <div class="footer">
     <div class="footer__flex">
       <div class="footer__info flex__box">
-        2018 (cc) example, by <a class="footer__info__link" href="" target="_blank">Dokodex</a>
+        2018 (cc) Gure Bazterrak, by <a class="footer__info__link" href="" target="_blank">Dokodex</a>
       </div>
       <div class="footer__rrss flex__box">
-        <a class="footer__rrss__link footer__rrss__link--facebook" href="" target="_blank"><?php require('imports/svg/facebook.svg') ?></a>
-        <a class="footer__rrss__link footer__rrss__link--instagram" href="" target="_blank"><?php require('imports/svg/instagram.svg') ?></a>
-        <a class="footer__rrss__link footer__rrss__link--twitter" href="" target="_blank"><?php require('imports/svg/twitter.svg') ?></a>
+        <a class="footer__rrss__link footer__rrss__link--facebook" href="https://www.facebook.com/gbazterrak/" target="_blank"><?php require('imports/svg/facebook.svg') ?></a>
+        <a class="footer__rrss__link footer__rrss__link--instagram" href="https://www.instagram.com/gurebazterrak/" target="_blank"><?php require('imports/svg/instagram.svg') ?></a>
+        <a class="footer__rrss__link footer__rrss__link--twitter" href="https://twitter.com/GureBazterrak" target="_blank"><?php require('imports/svg/twitter.svg') ?></a>
       </div>
     </div>
   </div>
